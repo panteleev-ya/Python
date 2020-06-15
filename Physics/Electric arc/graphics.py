ca34c162adccb46f9f25d5a68bddf4e0ae4581cc
SmartOven <yarik.panteleev@gmail.com>
2020-06-15 17:59:47 +0300

    update

--- Physics/Electric arc/graphics.py
+++ Physics/Electric arc/graphics.py
@@ -1,6 +1,10 @@
 import pygame
-from physics import Ladder
 from pygame.locals import *
+from physics import Ladder
+from math import sqrt
+from math import tan
+from math import sin
+from math import pi
 
 
 def input_from_file(filename):
@@ -38,14 +42,35 @@ def input_from_file(filename):
     return ladder_object
 
 
+def calculate_lines(angle, start_pos1, start_pos2, length):
+    L = length
+
+    q = (angle * pi / 180) / 2 # pi / 2 - (angle * pi / 180) / 2  # alpha / 2
+    tg_q = tan(q)
+    delta_x = round(abs(L * tg_q / sqrt(tg_q**2 + 1)))  # a1 + pixel_len * sin(q1)  # (a * tanq2 + a + sqrt(tanq2 * (tanq2 + 1))) / (tanq2 + 1)
+    delta_y = round(sqrt(L**2 - delta_x**2))  # b1 + sqrt(pixel_len**2 - (x1 - a1)**2)  # (b * sqrt(tanq2 * (tanq2 + 1)) + tan(q))/sqrt(tanq2 * (tanq2 + 1))
+
+    left_line = [start_pos1[0], start_pos1[1], start_pos1[0] - delta_x, start_pos1[1] - delta_y]
+    right_line = [start_pos2[0], start_pos2[1], start_pos2[0] + delta_x, start_pos2[1] - delta_y]
+
+    return left_line, right_line
+
+
 def graphics():
 
-    slow = 10  # 20 * 10 ** 6  # замедление времени
+    slow = 5  # 20 * 10 ** 6  # замедление времени
     correction_factor = 1.43  # коэффициент, умножение на который даст сильно приближенное к реальному обновление времени в условиях программы, так как расчтеты производятся не мгновенно
     precision = 3  # точность вывода, количество знаков после запятой при выводе (рассчеты все равно проводятся с максимально возможной для Python3 точностью)
-
+    start_pos_left = [774, 590]
+    start_pos_right = [814, 590]
+    meter_div_by_pix = 500  # количество пикселей в одном метре
     # Объявляем объект лестницы
     ladder = input_from_file('input.txt')
+    # Делаем стартовые рассчеты
+    ladder.make_start_calculations()
+    left_line, right_line = calculate_lines(ladder.stick_angle, start_pos_left, start_pos_right, meter_div_by_pix)
+    # print(left_line)
+    # print(right_line)
 
     # Подготовка для работы с графикой
     window_size = (1200, 675)
@@ -60,6 +85,7 @@ def graphics():
 
     # Переменные для графики
     BLACK = (0, 0, 0)
+    PURPLE = (163, 73, 164)
     font = pygame.font.SysFont('inconsolata', 30)
 
     # Переменная времени, в секундах
@@ -79,14 +105,26 @@ def graphics():
         screen.blit(world_bg_img, (0, 0))
         screen.blit(font.render(f"t = {round(t, precision)} c", 1, BLACK), (55, 225))
 
+        # Добавляем проводники, расположенные под углом stick_angle между ними
+        pygame.draw.line(screen, BLACK, [left_line[0], left_line[1]], [left_line[2], left_line[3]], 3)
+        pygame.draw.line(screen, BLACK, [right_line[0], right_line[1]], [right_line[2], right_line[3]], 3)
+
         if started:
             # Добавляем на экран дугу (или не добавляем)
-            # screen.blit(particle.image, (int(particle.current_position[0]), int(particle.current_position[1])))
+            print("passed:", ladder.stick_passed_d)
+            print("h:", ladder.arc_h)
+            pygame.draw.line(screen, PURPLE,
+                             [round(ladder.arc_start_position[0] - ladder.stick_passed_d * meter_div_by_pix),
+                              round(ladder.arc_start_position[1] + ladder.arc_h * meter_div_by_pix)
+                              ],
+                             [round(ladder.arc_start_position[0] + ladder.stick_passed_d * meter_div_by_pix),
+                              round(ladder.arc_start_position[1] + ladder.arc_h * meter_div_by_pix)
+                              ], 2)
 
             # Добавляем информацию о ней
-            writable = ladder.get_info()
-            for i in range(len(writable)):
-                screen.blit(font.render(writable[i], 1, BLACK), (55, 250 + i * 25))
+            # writable = ladder.get_info()
+            # for i in range(len(writable)):
+            #     screen.blit(font.render(writable[i], 1, BLACK), (55, 250 + i * 25))
 
             # Обновляем ее состояние
             ladder.make_regular_calculations(t)
@@ -97,7 +135,7 @@ def graphics():
             # t += 1
             t += (delay / 1000) * correction_factor / slow  # синхронизируемся с реальным временем
         else:
-            screen.blit(start_img, (100, 360))
+            screen.blit(start_img, (80, 360))
 
 
 if __name__ == '__main__':
--- Physics/Electric arc/physics.py
+++ Physics/Electric arc/physics.py
@@ -10,7 +10,8 @@ class Ladder:
 
 # Заранее известные константы
     # Общие константы
-    dielectric_strength = 3 * 10 ** 6  # Электрическая прочность воздушного промежутка (В/м)
+    arc_a = -9.81  # Ускорение дуги по оси Y (масса плазмы отрицательна)
+    dielectric_strength = 3 * 10 ** 6  # Электрическая прочность воздушного промежутка (В/м) 3000в/мм => 15000/х = 3000 => х = 5см, а в Галилео было около 3см разница, но дуга все равно разрывалась
     gas_R = 8.31446  # Универсальная газовая постоянная (Дж/(моль*К))
     g = 9.81  # Ускорение свободного падения (м/с^2)
     ion_y = 5 * 10 ** (-2)  # Коэффициент вторичной ионизации
@@ -18,11 +19,11 @@ class Ladder:
     # Все про воздух
     atmosphere_pressure = 10 ** 5  # Абсолютное давление (Па)
     air_temperature = 273.15  # Температура воздуха (K) (0 *С)
-    air_M = 29 * 10**(-3)  # Молярная масса воздуха (кг/моль)
+    # air_M = 29 * 10**(-3)  # Молярная масса воздуха (кг/моль)
 
     # Все про дугу
     arc_temperature = 9273.15  # Температура плазмы (К) (9000 *C)
-    arc_M = 29 * 10**(-3)  # Молярная масса ионизированного воздуха (кг/моль)
+    # arc_M = 29 * 10**(-3)  # Молярная масса ионизированного воздуха (кг/моль)
     stick_length = 1  # Длина проводников (м)
 
 # Входные данные
@@ -32,7 +33,7 @@ class Ladder:
     stick_p = 0.015  # Удельное сопротивление проводников - [0.015; 1.500] (в Ом*мм^2/м)
     stick_S = 0.5  # Площадь поперечного сечения провода - [0.5; 1600.0] (в мм^2) (до десятых)
 
-    # Все про аккумулятор
+    # Все про источник питания
     battery_U = 0  # Напряжение на выходе из источника питания - [0; 10000] (в Вольтах) (до целых)
     battery_I = 0  # Сила тока источника - [0.0, 10.0] (в Амперах) (до десятых)
 
@@ -43,35 +44,35 @@ class Ladder:
 
 # Вычисленные, не изменяющиеся данные
     # Все про провода
-    wire_R = wire_p * wire_length / wire_S                                                                              # (R = p*l/S) -> сопротивление каждого провода (они оба одинаковые)
-    wire_Q_dt = (battery_I ** 2) * wire_R                                                                               # (Q/t = I^2*R) -> выделяемое количество теплоты на каждом проводе в секунду
-    wire_U = battery_I * wire_R                                                                                         # (U = I*R) -> напряжение на каждом проводе (В)
+    wire_R = 0  # wire_p * wire_length / wire_S                                                                              # (R = p*l/S) -> сопротивление каждого провода (они оба одинаковые)
+    wire_Q_dt = 0  # (battery_I ** 2) * wire_R                                                                               # (Q/t = I^2*R) -> выделяемое количество теплоты на каждом проводе в секунду
+    wire_U = 0  # battery_I * wire_R                                                                                         # (U = I*R) -> напряжение на каждом проводе (В)
 
     # Все про воздух
-    air_p = atmosphere_pressure * air_M / (gas_R * air_temperature)                                                     # (P = (p*M)/(R*T)) -> плотность воздуха (кг/м^3)
+    # air_p = 0  # atmosphere_pressure * air_M / (gas_R * air_temperature)                                                     # (P = (p*M)/(R*T)) -> плотность воздуха (кг/м^3)
 
     # Все про дугу
-    arc_start_U = battery_U - 2 * wire_U                                                                                # (U = Uобщ - Uп1 - Uп2 = Uобщ - 2*Uп) -> начальное напряжение эл. дуги
-    arc_start_E = arc_start_U / stick_d                                                                                 # (E = U/d) -> начальная напряженность поля, образовавшегося между проводниками
+    arc_start_U = 0  # battery_U - 2 * wire_U                                                                                # (U = Uобщ - Uп1 - Uп2 = Uобщ - 2*Uп) -> начальное напряжение эл. дуги
+    arc_start_E = 0  # arc_start_U / stick_d                                                                                 # (E = U/d) -> начальная напряженность поля, образовавшегося между проводниками
     arc_h = 0                                                                                                           # (h = at^2/2, a - ускорение дуги, h0 = 0, v0 = 0) -> высота, на которую поднялась дуга
-    arc_p = atmosphere_pressure * arc_M / (gas_R * arc_temperature)                                                     # (P = (p*M)/(R*T)) -> плотность дуги (кг/м^3)
-    arc_start_position = [695, 613]                                                                                     # стартовая позиция дуги == [середина между проводниками, самая нижняя точка между проводниками]
-    arc_a = (air_p / arc_p - 1) * g                                                                                     # (ma = pgV - mg, p - плотность воздуха => a = (p1/p2 - 1)g) -> ускорение дуги (по оси Y)
-    arc_max_h = "undefined"  # cos((stick_angle * pi / 180) / 2)                                                        # h_max = cos(a/2) -> максимальная высота подъема дуги
-    ion_a = 1                                                                                                           # (0.2 * (E - 24.5)^2) -> Коэффициент ударной ионизации
+    # arc_p = 0  # atmosphere_pressure * arc_M / (gas_R * arc_temperature)                                                     # (P = (p*M)/(R*T)) -> плотность дуги (кг/м^3)
+    arc_start_position = 0  # [794, 590]                                                                                     # стартовая позиция дуги == [середина между проводниками, самая нижняя точка между проводниками]
+    # arc_a = -g  # (air_p / arc_p - 1) * g                                                                                     # (ma = pgV - mg, p - плотность воздуха => a = (p1/p2 - 1)g) -> ускорение дуги (по оси Y)
+    arc_max_h = 0  # "undefined"  # cos((stick_angle * pi / 180) / 2)                                                        # h_max = cos(a/2) -> максимальная высота подъема дуги
+    ion_a = 0  # 1                                                                                                           # (0.2 * (E - 24.5)^2) -> Коэффициент ударной ионизации
 
     # Условие образования дуги
-    required_stick_U = dielectric_strength * stick_d                                                                    # (Up = Ep/d) -> мин. напряжение, при котором образуется эл. дуга
+    required_stick_U = 0  # dielectric_strength * stick_d                                                                    # (Up = Ep/d) -> мин. напряжение, при котором образуется эл. дуга
 # Обновляющиеся данные
     # Все про проводники
-    stick_passed_length = arc_h / cos((stick_angle * pi / 180) / 2)                                                     # (length = h/cos(a/2)) -> длина проводника, которое дуга уже преодолела
-    stick_passed_d = stick_d + 2 * arc_h * tan((stick_angle * pi / 180) / 2)                                                      # (2*d = h * tg(a/2)) -> расстояние между проводниками на данной высоте
-    stick_passed_R = stick_p * stick_passed_length / stick_S                                                            # (R = p*l/S) -> сопротивление участка проводника, который теперь тоже является участком электрической цепи
-    stick_passed_U = battery_I * stick_passed_R                                                                         # (U = I*R) -> напряжение на каждом проводнике (В)
+    stick_passed_length = 0  # arc_h / cos((stick_angle * pi / 180) / 2)                                                     # (length = h/cos(a/2)) -> длина проводника, которое дуга уже преодолела
+    stick_passed_d = 0  # stick_d + 2 * arc_h * tan((stick_angle * pi / 180) / 2)                                                      # (2*d = h * tg(a/2)) -> расстояние между проводниками на данной высоте
+    stick_passed_R = 0  # stick_p * stick_passed_length / stick_S                                                            # (R = p*l/S) -> сопротивление участка проводника, который теперь тоже является участком электрической цепи
+    stick_passed_U = 0  # battery_I * stick_passed_R                                                                         # (U = I*R) -> напряжение на каждом проводнике (В)
 
     # Все про дугу
-    arc_position = [695, 613]                                                                                           # стартовая позиция дуги == [середина между проводниками, самая нижняя точка между проводниками]
-    arc_U = battery_U - 2 * (wire_U + stick_passed_U)                                                                   # (U = Uобщ - 2*U_w - 2*U_s) -> напряжение дуги
+    arc_position = 0  # arc_start_position                                                                                   # стартовая позиция дуги == [середина между проводниками, самая нижняя точка между проводниками]
+    arc_U = 0  # battery_U - 2 * (wire_U + stick_passed_U)                                                                   # (U = Uобщ - 2*U_w - 2*U_s) -> напряжение дуги
     arc_E = 0  # arc_U / stick_passed_length                                                                            # (E = U/d)
 
     # Конструктор с заданием начальных значений величин
@@ -86,8 +87,6 @@ class Ladder:
         self.stick_p = stick_p
         self.stick_S = stick_S
         self.ion_y = y
-        # Делаем стартовые рассчеты
-        self.make_start_calculations()
 
     # Округление полученных данных
     def round_data(self):
@@ -95,7 +94,7 @@ class Ladder:
             round(self.arc_position[0], self.precision),
             round(self.arc_position[1], self.precision)
         ]
-        self.arc_a = round(self.arc_a, self.precision)
+        # self.arc_a = round(self.arc_a, self.precision)
         # return [
         #     f"U: {self.battery_U}",
         #     f"I: {self.battery_I}",
@@ -116,16 +115,16 @@ class Ladder:
         self.wire_Q_dt = (self.battery_I ** 2) * self.wire_R                                                            # (Q/t = I^2*R) -> выделяемое количество теплоты на каждом проводе в секунду
         self.wire_U = self.battery_I * self.wire_R                                                                      # (U = I/R) -> напряжение на каждом проводе (В)
 
+        # Все про воздух
+        # self.air_p = self.atmosphere_pressure * self.air_M / (self.gas_R * self.air_temperature)                        # (P = (p*M)/(R*T)) -> плотность воздуха (кг/м^3)
+
         # Все про дугу
         self.arc_start_U = self.battery_U - 2 * self.wire_U                                                             # (U = Uобщ - Uп1 - Uп2 = Uобщ - 2*Uп) -> начальное напряжение эл. дуги
         self.arc_start_E = self.arc_start_U / self.stick_d                                                              # (E = U/d) -> начальная напряженность поля, образовавшегося между проводниками
-        self.arc_p = self.atmosphere_pressure * self.arc_M / (self.gas_R * self.arc_temperature)                        # (P = (p*M)/(R*T)) -> плотность дуги (кг/м^3)
-        self.arc_start_position = [695, 613]                                                                            # стартовая позиция дуги == [середина между проводниками, самая нижняя точка между проводниками]
-        self.arc_a = (self.air_p / self.arc_p - 1) * self.g                                                             # (ma = pgV - mg, p - плотность воздуха => a = (p1/p2 - 1)g) -> ускорение дуги (по оси Y)
-        self.arc_max_h = cos((self.stick_angle * pi / 180) / 2)                                                         # h_max = cos(a/2), если  -> максимальная высота подъема дуги
-
-        # Все про воздух
-        self.air_p = self.atmosphere_pressure * self.air_M / (self.gas_R * self.air_temperature)                        # (P = (p*M)/(R*T)) -> плотность воздуха (кг/м^3)
+        # self.arc_p = self.atmosphere_pressure * self.arc_M / (self.gas_R * self.arc_temperature)                        # (P = (p*M)/(R*T)) -> плотность дуги (кг/м^3)
+        self.arc_start_position = [794, 590]                                                                            # стартовая позиция дуги == [середина между проводниками, самая нижняя точка между проводниками]
+        # self.arc_a = (self.air_p / self.arc_p - 1) * self.g                                                             # (ma = pgV - mg, p - плотность воздуха => a = (p1/p2 - 1)g) -> ускорение дуги (по оси Y)
+        # self.arc_max_h = cos((self.stick_angle * pi / 180) / 2)                                                         # h_max = cos(a/2), если  -> максимальная высота подъема дуги
 
         # Условие образования дуги
         self.required_stick_U = self.dielectric_strength * self.stick_d                                                 # (Up = Ep/d) -> мин. напряжение, при котором образуется эл. дуга
@@ -164,14 +163,14 @@ class Ladder:
     # Передать информацию в виде списка
     def get_info(self):
         return [
-            f"U: {round(self.battery_U, self.output_precision)}",
-            f"I: {round(self.battery_I, self.output_precision)}",
-            f"Wires -> R: {round(self.wire_R, self.output_precision)}",
-            f"Wires -> Q/t: {round(self.wire_Q_dt, self.output_precision)}",
-            f"Arc -> U: {round(self.arc_U, self.output_precision)}",
-            f"Arc -> length: {round(self.stick_passed_length, self.output_precision)}",
-            f"Arc -> E: {round(self.arc_E, self.output_precision)}",
-            f"Arc -> H_max: {round(self.arc_max_h, self.output_precision)}"
+            ["Battery -> U", round(self.battery_U, self.output_precision)],
+            ["Battery -> I", round(self.battery_I, self.output_precision)],
+            ["Wires -> R", round(self.wire_R, self.output_precision)],
+            ["Wires -> Q/t", round(self.wire_Q_dt, self.output_precision)],
+            ["Arc -> U", round(self.arc_U, self.output_precision)],
+            ["Arc -> E", round(self.arc_E, self.output_precision)],
+            ["Arc -> length", round(self.stick_passed_length, self.output_precision)],
+            ["Arc -> H_max", round(self.arc_max_h, self.output_precision)]
         ]
 
     def set_precision(self, precision):

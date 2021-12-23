85d8217abb26a39b522d2b2f3307f892861c649c
Yaroslav Panteleev <31574114+SmartOven@users.noreply.github.com>
2021-12-23 21:12:59 +0300

    updated output format

--- Calculator/no_graphics_calc.py
+++ Calculator/no_graphics_calc.py
@@ -6,8 +6,8 @@ print(text)
 expression = input("Enter the math expression: ")
 while expression != "quit":
     try:
-        result = str(eval(expression))
+        result = "Result is: " + str(eval(expression))
     except:
         result = "Expression format mistake or division by zero"
-    print("Result is: " + result)
+    print(result)
     expression = input("Enter the math expression: ")

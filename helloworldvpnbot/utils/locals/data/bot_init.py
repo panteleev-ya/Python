41e905dba933f6aa69ec209a9264b1691289455b
Yaroslav Panteleev <yarik.panteleev@gmail.com>
2022-04-19 13:58:17 +0300

    Fixed "Token is invalid! It can't contain spaces" error

--- helloworldvpnbot/utils/locals/data/bot_init.py
+++ helloworldvpnbot/utils/locals/data/bot_init.py
@@ -3,10 +3,10 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 # Reading bot TOKEN from file
-token = open("utils/token.txt", "r").read()
+token = open("utils/token.txt", "r").read().split()[0]
 
 # Using test TOKEN instead while testing
-# token = open("utils/test_token.txt", "r").read()
+# token = open("utils/test_token.txt", "r").read().split()[0]
 
 # Memory storage initializing
 memory_storage = MemoryStorage()

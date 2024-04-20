9065b2c5c1d3e95f490a0f9bd33d864056bd4266
smartoven <yarik.panteleev@gmail.com>
2024-04-20 14:08:24 +0300

    v1.33

--- FormFiller/main.py
+++ FormFiller/main.py
@@ -171,7 +171,8 @@ async def input_purpose(message: types.Message, state: FSMContext):
         "User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"
     }
 
-    _ = requests.post(form_response_url, data=form_data, headers=user_agent)
+    response = requests.post(form_response_url, data=form_data, headers=user_agent)
+    print(str(response.status_code), str(response.text))
     await message.reply(f"Покупка зарегистрирована\n{datetime.now()}\n\n{purchase}")
     await delete_previous_user_messages(message.chat.id)
     await state.finish()
